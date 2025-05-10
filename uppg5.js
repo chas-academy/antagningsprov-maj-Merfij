90a2b088d0bd136fb20b75f12a4113ec94b7ab23
kristaps03@yahoo.co.uk <kristaps03@yahoo.co.uk>
2025-05-10 18:39:35 +0200

    Snälla låt detta fungera...jag har hållt på i 6 timmar med försöka ladda upp...

--- uppg5.js
+++ uppg5.js
@@ -3,6 +3,8 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+    let number;
+    let numberTwo;
     if (number > numberTwo){
         console.log("Det första talet är större än det andra")
     } else {
